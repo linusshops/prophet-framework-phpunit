a2f040fbd3b271cf4441e6b7d58913f4d15349b9
Sam Schmidt <samuel@dersam.net>
2016-04-01 11:44:42 -0400

    Initialize module for testing with init by framework.
    Resolves #21.

--- framework.php
+++ framework.php
@@ -6,11 +6,38 @@
  * @since 2016-04-01
  */
 
+$xml = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+
+<phpunit colors="true">
+    <testsuites>
+        <testsuite name="Prophet Test Suite">
+            <directory suffix="Test.php">./tests/</directory>
+        </testsuite>
+    </testsuites>
+
+    <filter>
+        <whitelist>
+            <directory>./src</directory>
+        </whitelist>
+    </filter>
+</phpunit>
+
+XML;
+
 return [
     'name' => 'phpunit',
     'validation' =>[
         'files' =>[
             'phpunit.xml'
+        ]
+    ],
+    'init' => [
+        'dirs' => [
+            './tests/phpunit'
+        ],
+        'files' => [
+            'phpunit.xml' => $xml
         ]
     ]
 ];
