36d126d3bfa8fc664957fa5d5c16f1a7f1e0562c
Sam Schmidt <samuel@dersam.net>
2016-04-01 11:02:35 -0400

    Allow configuration of frameworks, move validation to framework level.

---
+++ framework.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ *
+ * @author Sam Schmidt <samuel@dersam.net>
+ * @since 2016-04-01
+ */
+
+return [
+    'name' => 'phpunit',
+    'validation' =>[
+        'files' =>[
+            'phpunit.xml'
+        ]
+    ]
+];
